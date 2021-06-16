03d2bf93dc658fbed6d8799e7072026f9fee334b
Soumik Mukherjee <77982862+itsmesoumik23@users.noreply.github.com>
2021-06-16 19:43:46 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import keep from "./img/keep.png"
+function Header() {
+  return (
+    <header>
+      <h1><img alt="" src={keep} />Keep</h1>
+    </header>
+  );
+}
+
+export default Header;
